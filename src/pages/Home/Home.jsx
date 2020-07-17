7f37737ae3eff1269d441c3a4f3b5db231b6c050
Steven <scarvajalusb@gmail.com>
2020-07-17 05:41:02 -0500

    fixed home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Home.css";
+import img from "../../utils/imgs/pngwave.png";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Component-home">
+      <div className="Personal-data">
+        <h1 className="Steven">Hola ! </h1>
+        <h1 className="Steven">Mi nombre es Steven</h1>
+        <h2 className="Profesion">Ingeniero de software</h2>
+        <h2 className="Universidad">
+          Séptimo semestre de la universidad San Buenaventura De Cali
+        </h2>
+        <br />
+        <p>Pagina creada con:</p>
+        <br />
+        <p>React / Html / Css / JavaScript</p>
+      </div>
+      <img src={img} alt="" />
     </div>
   );
 };
